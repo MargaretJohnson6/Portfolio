d22bda61295412dbf7f98aa956b46e9227f5f5d2
MargaretJohnson6 <margaret.johnson@edu.sait.ca>
2023-11-28 09:51:19 -0700

    Javascript header and footer, favicon

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("hello")
-//load header info
+
 function addHeader(){
       //get the header where all this will go
       const header = document.getElementById("header");
@@ -11,33 +10,47 @@ function addHeader(){
 
       //Navigation
       let navi = document.createElement("nav");
-      let navList = document.createElement("ul");
       //links to the parts of the site
       let indexLink = document.createElement("a");
-      indexLink.href = "./index.html";
-      indexLink.innerHTML = "Home";
+            indexLink.href = "./index.html";
+            indexLink.innerHTML = "Home";
       let aboutMeLink = document.createElement("a");
-      aboutMeLink.href = "./about me.html";
-      aboutMeLink.innerHTML = "About Me";
+            aboutMeLink.href = "./about me.html";
+            aboutMeLink.innerHTML = "About Me";
       let galleryLink = document.createElement("a");
-      galleryLink.href = "./gallery.html";
-      galleryLink.innerHTML = "Gallery"
-      //place them
+            galleryLink.href = "./gallery.html";
+            galleryLink.innerHTML = "Gallery";
+      //place them in the header
       header.appendChild(icon);
-  	header.appendChild(navi);
-      navi.appendChild(navList);
-      navList.appendChild(indexLink);
-      navList.appendChild(aboutMeLink);
-      navList.appendChild(galleryLink);
+  	header.appendChild(navi); //navi floats to the right
+      navi.appendChild(indexLink);
+      navi.appendChild(aboutMeLink);
+      navi.appendChild(galleryLink);
+      console.log("header")
 }
 
-
-//load footer info
 function addFooter(){
       const footer = document.getElementById("footer")
       const today = new Date()
-      //call functions
       
+      let copyrightText = document.createElement("p");
+      copyrightText.innerHTML = "&copy;2023 Margaret Johnson. ";
+      let contactLink = document.createElement("a");
+      contactLink.href = "https://www.linkedin.com/in/margaret-johnson-450b06288/";
+      contactLink.innerHtml = "Linkedin, ";
+      let connectLink = document.createElement("a");
+      connectLink.href = ""; //TODO
+      connectLink.innerHTML = "Instagram. ";
+      let toTop = document.createElement("p");
+      toTop.href = "#header";
+      toTop.innerHTML = "Top";
+
+      //place them inside
+      footer.appendChild(copyrightText);
+      footer.appendChild(contactLink);
+      footer.appendChild(connectLink);
+      footer.appendChild(toTop);
+      console.log("footer")
 }
 addHeader()
 addFooter()
